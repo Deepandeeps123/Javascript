a003afa2ff6ba241310a692dfdb14fa980e8b1e0
Deepandeeps123 <deepanvinayagam1411@gmail.com>
2024-12-11 17:41:32 +0530

    first commit

---
+++ Pratice/pratice.js
@@ -0,0 +1,117 @@
+// let a=10
+// console.log(a);
+
+
+// let b=200
+// console.log(a)
+
+
+//! let num=10
+// let num1=10.10
+// console.log(num);
+// console.log(typeof num);
+
+// console.log(num1);
+// console.log(typeof num1);
+
+// let num3=Number(30)
+// console.log(typeof num3);
+
+
+
+
+// let str=true
+// console.log(str);
+// console.log(typeof str);
+
+
+// let str1 = false
+// console.log(str1);
+// console.log(typeof str1);
+
+
+
+// var  str='Hello'
+// console.log(str);
+// console.log(typeof str);
+
+// var str="Hello"
+// console.log(str);
+// console.log(typeof str);
+
+
+// var str=`Hello`
+// console.log(str);
+// console.log(typeof str);
+
+
+
+// let str = new String('Hello')
+// console.log(str);
+// console.log(typeof str);
+
+
+// let a
+// console.log(a);
+// console.log(typeof a);
+
+
+// let b = null
+// console.log(b);
+// console.log(typeof b);
+
+
+
+
+// let str=1234567889900097654,ll,kk
+// console.log(str);
+// console.log(typeof str);
+
+
+
+
+// let arr = [1,1.0,true,undefined]
+// console.log(arr);
+// console.log(typeof arr);
+
+
+
+
+
+// function add()
+// {
+//     console.log('I am add function');   
+// }
+// add()
+
+
+
+
+// let student = {
+//     name:'Dinga',
+//     age:22,
+//     ph:1234567890,
+//     skills:['html','css','js']
+
+// }
+// console.log(student);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
